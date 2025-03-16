1ca24300291ad907e02ca3cb9d8ce24b0dcc223a
kirsh_nat <nataliakirsh@yandex.ru>
2025-03-16 19:19:57 +0400

    mk iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,12 +4,29 @@ import (
 	"net/http"
 
 	"github.com/kirsh-nat/shortener.git/internal/app"
+	"go.uber.org/zap"
 )
 
+// var sugar zap.SugaredLogger
+
 func main() {
-	app.SetAppConfig()
-	if err := run(); err != nil {
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
 		panic(err)
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	// TODO: возмлонжно вообще вынести в конфигу аппы????
+	app.Sugar = *logger.Sugar()
+
+	app.SetAppConfig()
+
+	if err := run(); err != nil {
+		app.Sugar.Fatalw(err.Error(), "event", "start server")
+		//panic(err)
 	}
 }
 
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,16 +2,26 @@ package app
 
 import (
 	"github.com/kirsh-nat/shortener.git/internal/config"
+	"go.uber.org/zap"
 )
 
 var (
 	AppSettings = new(config.Config)
 	Store       *URLStore
+	Sugar       zap.SugaredLogger
 )
+
+//TODO: метод на объявление логгера и записи логов в файлик!!!!
 
 func SetAppConfig() {
 	AppSettings = new(config.Config)
 	Store = NewURLStore()
 	config.ParseFlags(AppSettings)
 	config.ValidateConfig(AppSettings)
+
+	Sugar.Infow(
+		"Starting server",
+		"addr", AppSettings.Addr,
+	)
+
 }
