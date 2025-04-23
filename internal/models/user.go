8716fccd79fdc8dbd425c71aff41a558097a599c
kirsh_nat <nataliakirsh@yandex.ru>
2025-04-23 18:24:10 +0400

    add user model

---
+++ internal/models/user.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UUID  string
+	Token string
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+	UserID string
+}
+
+const TOKEN_EXP = time.Hour * 3
+const SECRET_KEY = "supersecretkey"
+
+func CreateUser(uuid string) (*User, error) {
+	token, err := buildJWTString(uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		UUID:  uuid,
+		Token: token,
+	}, nil
+}
+
+func GetUser(token string) (User, error) {
+	userID, err := getUserID(token)
+	if err != nil {
+		return User{}, errors.New("user not found")
+	}
+
+	return User{UUID: userID, Token: token}, nil
+}
+
+func buildJWTString(UUID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TOKEN_EXP)),
+		},
+		UserID: UUID,
+	})
+
+	tokenString, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func getUserID(tokenString string) (string, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(SECRET_KEY), nil
+		})
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", err
+	}
+
+	return claims.UserID, nil
+}
+
+func GenerateUUID() string {
+	return uuid.New().String()
+}
