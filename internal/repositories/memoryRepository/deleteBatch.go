c25f0bb8e12ba6a810b9d45138e6a6330f9ee59b
kirsh_nat <nataliakirsh@yandex.ru>
2025-04-28 13:02:44 +0400

    make memory iter15

--- internal/repositories/memoryRepository/deleteBatch.go
+++ internal/repositories/memoryRepository/deleteBatch.go
@@ -1,16 +1,32 @@
 package memoryrepository
 
-func (r *MemoryRepository) DeleteBatch(shortURLs []string, userID string) {
-	for _, shortRequest := range shortURLs {
-		if _, ok := r.store[shortRequest]; !ok {
-			continue
-		}
-		if r.store[shortRequest].UserID != userID {
-			continue
-		}
+import "sync"
 
-		urlData := r.store[shortRequest]
-		urlData.Deleted = true
-		r.store[shortRequest] = urlData
+func (r *MemoryRepository) DeleteBatch(shortURLs []string, userID string) {
+	var wg sync.WaitGroup
+	results := make(chan string)
+
+	for _, shortRequest := range shortURLs {
+		wg.Add(1)
+		go func(shortRequest string) {
+			defer wg.Done()
+			r.mu.Lock()
+			defer r.mu.Unlock()
+
+			if urlData, ok := r.store[shortRequest]; ok && urlData.UserID == userID {
+				urlData.Deleted = true
+				r.store[shortRequest] = urlData
+				results <- shortRequest
+			}
+		}(shortRequest)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for result := range results {
+		println("Deleted:", result)
 	}
 }
--- internal/repositories/memoryRepository/get.go
+++ internal/repositories/memoryRepository/get.go
@@ -8,6 +8,10 @@ import (
 
 func (r *MemoryRepository) Get(_ context.Context, short string) (string, error) {
 	if val, ok := r.store[short]; ok {
+		if val.Deleted {
+			return val.OriginalURL, domain.NewDeletedError("URL deleted", nil)
+
+		}
 		return val.OriginalURL, nil
 	}
 
